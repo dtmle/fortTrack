b0f0fb8e0eff2d64829982c0f907a3d07501dfeb
Unknown <dennistmle@gmail.com>
2018-09-19 12:09:25 -0700

    Input tests

---
+++ src/input/Buttons.test.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { mount, render, shallow } from "enzyme";
+import { InputField, SendButton, AddButton, RemoveButton } from "./Buttons";
+
+describe("InputField", () => {
+  it("Should render", () => {
+    const component = shallow(<InputField />);
+    expect(component).toMatchSnapshot();
+  });
+  it("Should have a value", () => {
+    const component = shallow(<InputField value="Dennis" />);
+    expect(component.props().value).toEqual("Dennis");
+  });
+  it("Should have border red if invalid", () => {
+    const component = shallow(<InputField valid={false} />);
+    expect(component.props().style.borderColor).toEqual("rgb(255, 0, 0)");
+  });
+  it("Should have border red if unchanged", () => {
+    const component = shallow(<InputField changed={false} />);
+    expect(component.props().style.borderColor).toEqual("rgb(255, 0, 0)");
+  });
+  it("Should have border green if valid & changed", () => {
+    const component = shallow(<InputField valid={true} changed={true} />);
+    expect(component.props().style.borderColor).toEqual("rgb(0, 255, 0)");
+  });
+});
+
+describe("SendButton", () => {
+  it("Should render", () => {
+    const component = shallow(<SendButton />);
+    expect(component).toMatchSnapshot();
+  });
+  it("Should render text", () => {
+    const component = shallow(<SendButton text={"Dennis"} />);
+    expect(component.props().value).toEqual("Dennis");
+  });
+});
+
+describe("AddButton", () => {
+  it("Should render", () => {
+    const component = shallow(<AddButton />);
+    expect(component).toMatchSnapshot();
+  });
+  it("Should be visible when count < 4", () => {
+    const component = shallow(<AddButton count={1} />);
+    expect(component.props().style.visibility).toEqual("visible");
+  });
+  it("Should be hidden when count == 4", () => {
+    const component = shallow(<AddButton count={4} />);
+    expect(component.props().style.visibility).toEqual("hidden");
+  });
+});
+
+describe("RemoveButton", () => {
+  it("Should render", () => {
+    const component = shallow(<RemoveButton />);
+    expect(component).toMatchSnapshot();
+  });
+});
